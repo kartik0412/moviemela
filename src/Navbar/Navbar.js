f338ba23d33b37846109a47d28b824a914cb5ee3
kartik <vermakartik222@gmail.com>
2020-08-02 16:18:51 +0530

    fix navbar bug

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -34,43 +34,6 @@ class Navbar extends React.Component {
     }
 
     render() {
-        // return (
-        //     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-        //         <a className="navbar-brand" href="/">
-        //             MovieMela
-        //         </a>
-        //         <button
-        //             className="navbar-toggler"
-        //             type="button"
-        //             data-toggle="collapse"
-        //             data-target="#navbarsExample03"
-        //             aria-controls="navbarsExample03"
-        //             aria-expanded="false"
-        //             aria-label="Toggle navigation"
-        //         >
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-
-        //         <div className="collapse navbar-collapse" id="navbarsExample03">
-        //             <ul className="navbar-nav mr-auto"></ul>
-        //             <form onSubmit={this.handleSubmit} className="form-inline my-2 my-md-0">
-        //                 <select onChange={this.handleChangeSelect} style={{ width: "112px" }} className="custom-select">
-        //                     <option defaultValue value="movie">
-        //                         Movie
-        //                     </option>
-        //                     <option value="tv">TV-Series</option>
-        //                 </select>
-        //                 <input
-        //                     onChange={this.handleChange}
-        //                     id="input"
-        //                     className="form-control"
-        //                     type="text"
-        //                     placeholder="Search"
-        //                 />
-        //             </form>
-        //         </div>
-        //     </nav>
-        // );
         return (
             <nav className='nav'>
                 <div className='logo'><a href="/">MovieMela</a></div>
