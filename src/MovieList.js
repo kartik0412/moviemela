d6f49fb26efdbfe5cf021367536fe4b25376b75c
kartik <vermakartik222@gmail.com>
2020-08-02 15:50:01 +0530

    new navbar

--- src/MovieList.js
+++ src/MovieList.js
@@ -26,7 +26,7 @@ export default class MovieList extends React.Component {
                 isloading: false,
                 movielist: movielist.data,
             });
-        } catch (err) {}
+        } catch (err) { }
     }
 
     nextpage() {
@@ -55,7 +55,7 @@ export default class MovieList extends React.Component {
     async componentDidMount() {
         try {
             this.getpage();
-        } catch (err) {}
+        } catch (err) { }
     }
 
     render() {
@@ -72,35 +72,35 @@ export default class MovieList extends React.Component {
                 {this.state.isloading ? (
                     <div className="loader"></div>
                 ) : (
-                    <>
-                        <h1 className="heading">Trending Movies</h1>
-                        <div className="main">
-                            {this.state.movielist.results.map((j, i) => (
-                                <Card
-                                    key={i}
-                                    type={"movie"}
-                                    title={j.title}
-                                    overview={j.overview ? j.overview.substr(0, 120) + ".." : ""}
-                                    year={j.release_date ? j.release_date.substr(0, 4) : ""}
-                                    imageurl={j.poster_path}
-                                    id={j.id}
-                                />
-                            ))}
-                        </div>
-                        <div className="page">
-                            {this.state.page - 1 >= 1 && (
-                                <button onClick={this.prevpage} className="prev">
-                                    PREV
-                                </button>
-                            )}
-                            {this.state.page + 1 <= this.state.movielist.total_pages && (
-                                <button onClick={this.nextpage} className="next">
-                                    NEXT
-                                </button>
-                            )}
-                        </div>
-                    </>
-                )}
+                        <>
+                            <h1 className="heading">Trending Movies</h1>
+                            <div className="main">
+                                {this.state.movielist.results.map((j, i) => (
+                                    <Card
+                                        key={i}
+                                        type={"movie"}
+                                        title={j.title}
+                                        overview={j.overview ? j.overview.substr(0, 120) + ".." : ""}
+                                        year={j.release_date ? j.release_date.substr(0, 4) : ""}
+                                        imageurl={j.poster_path}
+                                        id={j.id}
+                                    />
+                                ))}
+                            </div>
+                            <div className="page">
+                                {this.state.page - 1 >= 1 && (
+                                    <button onClick={this.prevpage} className="prev">
+                                        PREV
+                                    </button>
+                                )}
+                                {this.state.page + 1 <= this.state.movielist.total_pages && (
+                                    <button onClick={this.nextpage} className="next">
+                                        NEXT
+                                    </button>
+                                )}
+                            </div>
+                        </>
+                    )}
             </div>
         );
     }
--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -24,45 +24,84 @@ class Navbar extends React.Component {
         this.props.handleSubmit(this.state.name);
     }
 
-    render() {
-        return (
-            <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-                <a className="navbar-brand" href="/">
-                    MovieMela
-                </a>
-                <button
-                    className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbarsExample03"
-                    aria-controls="navbarsExample03"
-                    aria-expanded="false"
-                    aria-label="Toggle navigation"
-                >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+    componentDidMount() {
+        var burger = document.getElementsByClassName('burger')[0];
+        var options = document.getElementsByClassName('options')[0];
+        burger.addEventListener("click", () => {
+            burger.classList.toggle('toggle');
+            options.classList.toggle('slide-options');
+        });
+    }
 
-                <div className="collapse navbar-collapse" id="navbarsExample03">
-                    <ul className="navbar-nav mr-auto"></ul>
-                    <form onSubmit={this.handleSubmit} className="form-inline my-2 my-md-0">
-                        <select onChange={this.handleChangeSelect} style={{ width: "112px" }} className="custom-select">
-                            <option defaultValue value="movie">
-                                Movie
-                            </option>
-                            <option value="tv">TV-Series</option>
-                        </select>
-                        <input
-                            onChange={this.handleChange}
-                            id="input"
-                            className="form-control"
-                            type="text"
-                            placeholder="Search"
-                        />
-                    </form>
+    render() {
+        // return (
+        //     <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
+        //         <a className="navbar-brand" href="/">
+        //             MovieMela
+        //         </a>
+        //         <button
+        //             className="navbar-toggler"
+        //             type="button"
+        //             data-toggle="collapse"
+        //             data-target="#navbarsExample03"
+        //             aria-controls="navbarsExample03"
+        //             aria-expanded="false"
+        //             aria-label="Toggle navigation"
+        //         >
+        //             <span className="navbar-toggler-icon"></span>
+        //         </button>
+
+        //         <div className="collapse navbar-collapse" id="navbarsExample03">
+        //             <ul className="navbar-nav mr-auto"></ul>
+        //             <form onSubmit={this.handleSubmit} className="form-inline my-2 my-md-0">
+        //                 <select onChange={this.handleChangeSelect} style={{ width: "112px" }} className="custom-select">
+        //                     <option defaultValue value="movie">
+        //                         Movie
+        //                     </option>
+        //                     <option value="tv">TV-Series</option>
+        //                 </select>
+        //                 <input
+        //                     onChange={this.handleChange}
+        //                     id="input"
+        //                     className="form-control"
+        //                     type="text"
+        //                     placeholder="Search"
+        //                 />
+        //             </form>
+        //         </div>
+        //     </nav>
+        // );
+        return (
+            <nav className='nav'>
+                <div className='logo'><a href="/">MovieMela</a></div>
+                <div className='options'>
+                    <ul className='nav-links'>
+                        <form onSubmit={this.handleSubmit} style={{ display: "inline-flex" }}>
+                            <li>
+                                <select onChange={this.handleChangeSelect} style={{ width: "102px" }} className="custom-select">
+                                    <option defaultValue value="movie">Movie</option>
+                                    <option value="tv">TV-Series</option>
+                                </select>
+                            </li>
+                            <li>
+                                <input
+                                    onChange={this.handleChange}
+                                    id="input"
+                                    className="form-control"
+                                    type="text"
+                                    placeholder="Search"
+                                />
+                            </li>
+                        </form>
+                    </ul>
+                </div>
+                <div className='burger'>
+                    <div className="line1"></div>
+                    <div className="line2"></div>
+                    <div className="line3"></div>
                 </div>
             </nav>
-        );
+        )
     }
 }
-
 export default Navbar;
